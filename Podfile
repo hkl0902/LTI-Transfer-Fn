cd8c0b6363259e8ea600c62ce8e9f36ba1b449e2
Hyunkyu Michael Lee <michael@getrocket.com>
2019-08-18 13:26:44 -0700

    remove unnecessary pods, use pure swift

--- Podfile
+++
@@ -1,6 +0,0 @@
-target 'LTI-Project' do
-  	platform :ios, '9.0'
-
-	use_frameworks!
-	pod 'CorePlot', '~>2.2'
-end
--- Pods/Target Support Files/Pods-LTI-Project/Pods-LTI-Project-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LTI_Project : NSObject
-@end
-@implementation PodsDummy_Pods_LTI_Project
-@end
